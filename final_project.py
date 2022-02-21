cdf697e321af6fc6ad397a81c0840022cacf0cde
kombattle <88496886+kombattle@users.noreply.github.com>
2022-02-21 23:37:30 +0700

    coding to connect sql with python

--- final_project.py
+++ final_project.py
@@ -1,22 +1,23 @@
 ## Final project - IT fundamentals
 ## By Ginna Phanvimolchai
-## 30 April 2020
+## 21 Feb 2022
 
 ## ---------------------------------------
 ## import sqlite3 library
-
+import sqlite3
 
 ## connect to database
-import sqlite3
 con = sqlite3.connect("university.db")
 
 ## create cursor to execute our queries
 cur = conn.cursor()
 
 ## write our queries
-
+sql = "select * from student limit 5;"
 
 ## fetch data
-
+cur.execute(sql)
+result = cur.fetchall()
 
 ## print result in console
+print(result)
