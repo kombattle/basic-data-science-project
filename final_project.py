cc614e068cfeb3d39d4e0a4ba37f125c0d3cd114
kombattle <88496886+kombattle@users.noreply.github.com>
2022-02-21 23:41:39 +0700

    change limit to 5 ppl

--- final_project.py
+++ final_project.py
@@ -10,10 +10,10 @@ import sqlite3
 con = sqlite3.connect("university.db")
 
 ## create cursor to execute our queries
-cur = conn.cursor()
+cur = con.cursor()
 
 ## write our queries
-sql = "select * from student limit 5;"
+sql = "select * from student join contact on student.id = contact.student_id limit 5;"
 
 ## fetch data
 cur.execute(sql)
